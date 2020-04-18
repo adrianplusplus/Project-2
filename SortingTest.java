ac2cad2df39886ff9b31bb53a2e9f00f333a64f1
DataLuminescence <61890852+DataLuminescence@users.noreply.github.com>
2020-04-17 18:58:46 -0700

    Add files via upload

---
+++ SortingTest.java
@@ -0,0 +1,74 @@
+package sorting;
+
+import java.util.LinkedList;
+
+class Node
+{
+	int data;
+	Node next;
+}
+
+class LinkedL
+{
+	
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+public class SortingTest<E> implements Sorting<E>
+{
+
+	public static void main(String[] args) 
+	{
+		
+		
+		
+		//insertionSort(null, 0, 0, false);
+
+	}
+
+	@Override
+	public void insertionSort(LinkedList<E> list, int lowindex, int highindex, boolean reversed) 
+	{
+		 
+		Node root = null;
+		LinkedL a = new LinkedL();
+		
+//		root = a.insert();
+//		root = a.insert();
+//		root = a.insert();
+		
+		
+		
+	}
+
+	@Override
+	public void quicksort(LinkedList<E> list, int lowindex, int highindex, boolean reversed) 
+	{
+		
+		
+		
+		
+	}
+
+	@Override
+	public void mergeSortLL(LinkedList<E> list, boolean reversed) 
+	{
+		
+		
+	}
+
+	
+
+}
