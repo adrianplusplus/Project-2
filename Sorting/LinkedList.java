7b91d2a2cf2b22d591b5c066bb18c5147483d4fa
Adrian Galicia <adrian.galicia92@gmail.com>
2020-04-18 04:24:37 -0500

    move to Sorted Package and adapt LinkedList to be generic

---
+++ Sorting/LinkedList.java
@@ -0,0 +1,89 @@
+package Sorting;
+
+// E needs to implement Comparable so we can compare with other objects
+public class LinkedList<E extends Comparable<? super E>> {
+    Node<E> head;
+    Node<E> sorted;
+
+    // T needs to implement Comparable so we can compare to other objects
+    class Node<T extends Comparable<? super T>> {
+        T val;
+        Node<T> next;
+
+        public Node(T val) {
+            this.val = val;
+        }
+    }
+
+    void push(E val) {
+        /* allocate node */
+        Node<E> newnode = new Node<E>(val);
+        /* link the old list off the new node */
+        newnode.next = head;
+        /* move the head to point to the new node */
+        head = newnode;
+    }
+
+    // function to sort a singly linked list using insertion sort
+    void insertionSort(Node<E> headref) {
+        // Initialize sorted linked list
+        sorted = null;
+        Node<E> current = headref;
+        // Traverse the given linked list and insert every
+        // node to sorted
+        while (current != null) {
+            // Store next for next iteration
+            Node<E> next = current.next;
+            // insert current in sorted linked list
+            sortedInsert(current);
+            // Update current
+            current = next;
+        }
+        // Update head_ref to point to sorted linked list
+        head = sorted;
+    }
+
+    /*
+     * function to insert a new_node in a list. Note that this function expects a
+     * pointer to head_ref as this can modify the head of the input linked list
+     * (similar to push())
+     */
+    void sortedInsert(Node<E> newnode) {
+        /* Special case for the head end */
+        if (sorted == null || newnode.val.compareTo(sorted.val) <= 0) {
+            newnode.next = sorted;
+            sorted = newnode;
+        } else {
+            Node<E> current = sorted;
+            /* Locate the node before the point of insertion */
+            while (current.next != null && current.next.val.compareTo(newnode.val) < 0) {
+                current = current.next;
+            }
+            newnode.next = current.next;
+            current.next = newnode;
+        }
+    }
+
+    /* Function to print linked list */
+    void printlist(Node<E> head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+
+    // Driver program to test above functions
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        list.push(5);
+        list.push(20);
+        list.push(4);
+        list.push(3);
+        list.push(30);
+        System.out.println("Linked List before Sorting..");
+        list.printlist(list.head);
+        list.insertionSort(list.head);
+        System.out.println("\nLinkedList After sorting");
+        list.printlist(list.head);
+    }
+}
